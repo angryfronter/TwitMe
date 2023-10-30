3406937a94a5c7856ef2aa8a7e77b1f90fdb508c
angryfronter <11022003slovo@gmail.com>
2023-10-30 18:52:47 +0300

    feat: create comments

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,4 +1,5 @@
 class Advertisement < ApplicationRecord
+  has_many :comments
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true, length: {minimum: 5}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_27_115536) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_30_142145) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,4 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_27_115536) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "advertisement_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["advertisement_id"], name: "index_comments_on_advertisement_id"
+  end
+
+  add_foreign_key "comments", "advertisements"
 end
