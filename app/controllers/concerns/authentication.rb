5c3fbe780336811f9b6157580fd85fbe075f1b65
angryfronter <11022003slovo@gmail.com>
2023-11-02 20:52:15 +0300

    feat: add roles and admin page for edit or delete users

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -7,7 +7,7 @@ module Authentication
     private
 
     def current_user
-      @current_user ||= User.find_by(id: session[:user_id]).decorate if session[:user_id].present?
+      @current_user ||= User.find_by(id: session[:user_id])&.decorate if session[:user_id].present?
     end
 
     def user_signed_in?
