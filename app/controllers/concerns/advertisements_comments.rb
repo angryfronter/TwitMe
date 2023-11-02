9eb46cedf487e4280d9d1cae432a82ec4e94d4d7
angryfronter <11022003slovo@gmail.com>
2023-11-02 17:59:02 +0300

    fix: fix 2 lines for better render

--- app/controllers/concerns/advertisements_comments.rb
+++ app/controllers/concerns/advertisements_comments.rb
@@ -5,7 +5,7 @@ module AdvertisementsComments
     def load_advertisement_comments(do_render: false)
       @advertisement = @advertisement.decorate
       @comment ||= @advertisement.comments.build
-      @comments = @advertisement.comments.order(created_at: :desc).decorate
+      @comments = @advertisement.comments.includes(:user).order(created_at: :desc).decorate
       render('advertisements/show') if do_render
     end
   end
