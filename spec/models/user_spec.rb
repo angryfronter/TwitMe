81410cb3fb8c2bc3fde17ac3419537654a7cf662
angryfronter <11022003slovo@gmail.com>
2023-11-08 13:23:17 +0300

    rspec(user): fix user_spec.rb test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe User, type: :model do
     user = build(:user, email: nil)
     expect(user).not_to be_valid
   end
+
+  it 'is not valid with a duplicate email' do
+    create(:user, email: 'test@example.com')
+    user = build(:user, email: 'test@example.com')
+    expect(user).not_to be_valid
+  end
+
+  it 'returns true for author? if the user is the author' do
+    advertisement = create(:advertisement)
+    user = advertisement.user
+    expect(user.author?(advertisement)).to be true
+  end
+
+  it 'returns false for author? if the user is not the author' do
+    user = create(:user)
+    advertisement = create(:advertisement)
+    expect(user.author?(advertisement)).to be false
+  end
 end
